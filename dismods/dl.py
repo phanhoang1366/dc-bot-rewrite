e8e8abcbe10677aa9044e7189baa8c39839ec2e3
phanhoang1366 <84622359+phanhoang1366@users.noreply.github.com>
2024-12-20 15:29:33 +0700

    Update dl.py

--- dismods/dl.py
+++ dismods/dl.py
@@ -2,25 +2,29 @@ import subprocess
 import os
 
 def ydl(url, option):
-
     if option == "list-formats" or option == "-F":
         cmdopt = "-F"
     elif option != "":
-        cmdopt = "-f \"" + option + "\""
+        cmdopt = f"-f {option}"
     else:
-        cmdopt = "-f \"18/sd/mp4+m4a/w\" -S vcodec:h264"
+        cmdopt = "-f 18/sd/mp4+m4a/w -S vcodec:h264"
         
-    cmd = "yt-dlp " + cmdopt + " \"" + url + "\" -o video.mp4 --max-filesize 25M --no-colors --user-agent \"Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5666.197 Safari/537.36\""
+    cmd = [
+        "yt-dlp", cmdopt, url, "-o", "video.mp4", "--max-filesize", "25M", 
+        "--no-colors", "--user-agent", 
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5666.197 Safari/537.36"
+    ]
     
-    print(cmd)
-    
+    print(" ".join(cmd))
+
     try:
-        output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=30)
     except subprocess.CalledProcessError as outputexc: 
         with open("output.txt", "w") as f:
             f.write(outputexc.output.decode('UTF-8'))
-            f.close()
         return 10
+    except subprocess.TimeoutExpired:
+        return 12
      
     if os.path.exists("video.mp4"):
         return 0
@@ -30,9 +34,7 @@ def ydl(url, option):
         except FileNotFoundError:
             pass
         return 1
-            
     else:
         with open("output.txt", "w") as f:
             f.write(output.decode('UTF-8'))
-            f.close()
         return 11
