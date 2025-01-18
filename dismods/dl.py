df26da98ca1f1692fd978546a77eb1a2f9e650d0
phanhoang1366 <84622359+phanhoang1366@users.noreply.github.com>
2025-01-19 01:00:27 +0700

    Update dl.py

--- dismods/dl.py
+++ dismods/dl.py
@@ -10,7 +10,7 @@ def ydl(url, option):
         cmdopt = "-f 18/sd/mp4+m4a/w -S vcodec:h264"
         
     cmd = [
-        "yt-dlp", cmdopt, url, "-o", "video.mp4", "--max-filesize", "25M", 
+        "yt-dlp", cmdopt, url, "-o", "video.mp4", "--max-filesize", "10M", 
         "--no-colors", "--user-agent", 
         "Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5666.197 Safari/537.36"
     ]
